e322abe9bf44659d2c73f265ea96a29b47053796
Dimpal Kalita <kalitadimpal112@gmail.com>
2023-06-26 19:28:01 +0530

    CREATE: signin sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
 import { Routes, Route } from "react-router-dom";
 
-import { Home, Error, Bot } from "./Pages";
+import { useRhinoValue } from "react-rhino";
 
-import { Navbar, Footer } from "./Components";
+import { Home, Error } from "./Pages";
+
+import { Footer } from "./Components";
+
+import Navbar from "./Components/LoggedOut/Navbar/Navbar";
+import Signin from "./Components/LoggedOut/Register/Signin";
+import Signup from "./Components/LoggedOut/Register/Signup";
+import ForgotPassword from "./Components/LoggedOut/Register/ForgotPassword";
 
 const App = () => {
+  const id = useRhinoValue("id");
+
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error />} />
-        <Route path="/bot" element={<Bot />} />
-      </Routes>
-      <Footer />
-    </>
+    <div>
+      {id === "" ? (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Error />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+          </Routes>
+          <Footer />
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
+    </div>
   );
 };
 
